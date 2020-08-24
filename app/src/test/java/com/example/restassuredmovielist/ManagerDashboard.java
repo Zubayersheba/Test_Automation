195f060b308ee109aa07dec1f1d5cabc66fb56cb
zubayersheba <zubayersheba18@gmail.com>
2020-08-24 12:24:35 +0600

    [sManager geo_info Problem solved]

--- app/src/test/java/com/example/restassuredmovielist/ManagerDashboard.java
+++ app/src/test/java/com/example/restassuredmovielist/ManagerDashboard.java
@@ -6,13 +6,14 @@ import org.junit.Test;
 import io.restassured.RestAssured;
 
 import static io.restassured.RestAssured.basePath;
+import static io.restassured.RestAssured.get;
 import static io.restassured.RestAssured.when;
 import static org.hamcrest.core.IsEqual.equalTo;
 
 public class ManagerDashboard {
     @Before
     public void setBaseURL(){
-        RestAssured.baseURI = "https://api.sheba.xyz";
+        RestAssured.baseURI = "https://api.dev-sheba.xyz";
     }
     @Test
     public void sManager_Dashboard_StatusCode_200()
@@ -37,14 +38,21 @@ public class ManagerDashboard {
                 .then().body("data.name",equalTo("Test.Sp"));
 
     }
+    @Test
+    public void sManager_Dashboard_data_badge()
+    {
+        when().
+                get("v2/partners/17903/dashboard?remember_token=aCwLKJ3zd4qcKdUfD6IqxAKFmEYz9nZDuxfZllQijcxzCNBZxfhpX6DkfH63NHxJgEgiyyACtyJHb3KzV9cxaxWEynqHUMocc0dLIrqr3EaHGzATXq3IhdrMFc8ZlbOPiTwTSnfqFzwHlhllnGk09uzNUaT5bHSOAB6GTkSnooqJInJxG3NF96McopCGsdVxqJWdtjEuPlqn3zmzdT5WIXCNd4tvefCtgXPbOng4O73ufyDGSv3ivfAUWEZtXt4")
+                .then().body("data.badge",equalTo(null));
+
+    }
 
 //current_subscription_package"
-@Test
-public void sManager_Dashboard_current_subscription_package()
-{
-    when().
-            get("v2/partners/17903/dashboard?remember_token=aCwLKJ3zd4qcKdUfD6IqxAKFmEYz9nZDuxfZllQijcxzCNBZxfhpX6DkfH63NHxJgEgiyyACtyJHb3KzV9cxaxWEynqHUMocc0dLIrqr3EaHGzATXq3IhdrMFc8ZlbOPiTwTSnfqFzwHlhllnGk09uzNUaT5bHSOAB6GTkSnooqJInJxG3NF96McopCGsdVxqJWdtjEuPlqn3zmzdT5WIXCNd4tvefCtgXPbOng4O73ufyDGSv3ivfAUWEZtXt4")
-            .then().body("data.geo_informations.lat",equalTo("23.790964"));
 
-}
+    @Test
+    public void current_subscription_packag() {
+        get("/v2/partners/37662/dashboard?remember_token=vUXmcCyv6MblL0fafVZuRjrQdEVKB6fXn7nyGttEmlEY8hRUSEpHvrGPjM59").
+                then().assertThat()
+                .body("data.geo_informations.lat", (equalTo("23.79534268524198")));
+    }
 }
